219f026b6b6112f141e08166b3545ddf23339843
Ondic <91017666+Ondic@users.noreply.github.com>
2021-10-21 10:38:11 +0200

    WebGl Upload

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject target;
+    public GameObject parentOfTargets;
+
+    public bool won;
+    public int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
+        won = false;
+        InvokeRepeating("Spawn", 1f, 2f);
 
-        Spawn();
-        Spawn();
-        Spawn();
-        Spawn();
     }
+
+    // Spawn a Target at a random position within a specified x and y range.
+    // Instantiate (makea a concrete GameObject, i.e. a clone from the given prefab target) the
+    // target as child of the parentOfTargets. In this case transform.localPosition instead of
+    // transform.position is important!!
     private void Spawn ()
     {
-        float randomX = Random.Range(-100, 100);
-        float randomY = Random.Range(-100, 100);
+        float randomX = Random.Range(-420, 421);
+        float randomY = Random.Range(-240, 241);
 
         Vector2 random2DPosition = new Vector2(randomX, randomY);
+
+        GameObject myTarget = Instantiate(target, parentOfTargets.transform);
+        myTarget.transform.localPosition = random2DPosition;
+
 
         Debug.Log(random2DPosition);
     }
@@ -31,6 +39,30 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(won == true)
+        {
+            CancelInvoke("Spawn");
+        }
+        else
+        {
+            Debug.Log(won);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse pressed");
+        }
+    }
+
+
+    public void IncrementScore()
+    {
+        score++;
+        Debug.Log("increment...." + score);
+
+        if(score > 10)
+        {
+            won = true;
+        }
     }
 }
